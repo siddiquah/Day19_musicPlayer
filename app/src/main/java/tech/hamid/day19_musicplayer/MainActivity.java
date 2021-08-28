675e220a1749a7bdf8d09b39c5e2868183b838ef
siddiquah anjum <siddiquahanjum111@gmail.com>
2021-08-28 15:55:04 +0530

    initial commit

---
+++ app/src/main/java/tech/hamid/day19_musicplayer/MainActivity.java
@@ -0,0 +1,34 @@
+package tech.hamid.day19_musicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    MediaPlayer music;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        music = MediaPlayer.create(this, R.raw.music);
+    }
+
+    public void musicplay (View v)
+    {
+        music.start();
+    }
+
+    public void musicpause (View v)
+    {
+        music.pause();
+    }
+
+    public void musicstop (View v)
+    {
+        music.stop();
+        music = MediaPlayer.create(this, R.raw.music);
+    }
+}
